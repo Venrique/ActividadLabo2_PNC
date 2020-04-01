8c33a59471cca8d2c9362161582e8f19af894bad
'venrique' <'00046517@uca.edu.sv'>
2020-04-01 17:53:55 -0600

    ActividadLabo2

---
+++ ActLabo2/src/main/java/com/uca/capas/ActLabo2Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActLabo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActLabo2Application.class, args);
+	}
+
+}
